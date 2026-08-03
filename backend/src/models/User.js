b031ef1553198aab65bbaad4e263d23867be2a8c
Natali258 <natalya.holyavko@gmail.com>
2026-08-03 19:13:50 +0300

    add User.js

---
+++ backend/src/models/User.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    phone: {
+      type: String,
+      trim: true,
+    },
+
+    avatar: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model("User", userSchema);
